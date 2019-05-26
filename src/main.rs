baa66491a2185141f981ebb2a6cd056cbfd6a436
Pascal Hartig <phartig@rdrei.net>
2019-05-26 11:54:09 +0100

    Project setup

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest, Responder};
+
+fn greet(req: &HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", to)
+}
+
+fn main() {
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(greet))
+            .resource("/{name}", |r| r.f(greet))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("Can not bind to port 8000")
+    .run();
+}
+
